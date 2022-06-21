dabc5795db6363cc996dea7862549e305d1bc247
Raman Charapennikau <charapennikaurm@gmail.com>
2022-06-21 19:28:52 +0300

    inference on aws lambda

--- src/lambda_inference/lambda_inference.py
+++ src/lambda_inference/lambda_inference.py
@@ -1,11 +1,46 @@
+import base64
+import json
 import os
+import time
 
-from inference_runner import InferenceRunner
+import boto3
+from data_processing import transform_dataset
+from pyspark.ml import PipelineModel
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, StructField, StructType
+
+COLUMNS = [
+    'customerID',
+    'gender',
+    'SeniorCitizen',
+    'Partner',
+    'Dependents',
+    'tenure',
+    'PhoneService',
+    'MultipleLines',
+    'InternetService',
+    'OnlineSecurity',
+    'OnlineBackup',
+    'DeviceProtection',
+    'TechSupport',
+    'StreamingTV',
+    'StreamingMovies',
+    'Contract',
+    'PaperlessBilling',
+    'PaymentMethod',
+    'MonthlyCharges',
+    'TotalCharges',
+]
 
 PATH_TO_MODEL = "s3a://test-iot-s3-bucket/test_model"
-KINESIS_STREAM = "telco-inference"
 S3_BUCKET = "test-iot-s3-bucket"
+
+
+def decode_message(base64_message: str):
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    message = message_bytes.decode('ascii')
+    return message
 
 
 def lambda_handler(event, context):
@@ -32,19 +67,32 @@ def lambda_handler(event, context):
             "spark.driver.extraJavaOptions", "-Dcom.amazonaws.services.s3.enableV4=true"
         )
         .config("spark.driver.bindAddress", "127.0.0.1")
+        .config("spark.driver.host", "127.0.0.1")
         .config("spark.shuffle.s3.enabled", True)
         .config("spark.lambda.concurrent.requests.max", 50)
         .config("spark.dynamicAllocation.enabled", True)
         .getOrCreate()
     )
 
-    runner = InferenceRunner(
-        spark=spark,
-        path_to_model=PATH_TO_MODEL,
-        kinesis_stream_name=KINESIS_STREAM,
-        s3_bucket_name=S3_BUCKET,
+    records = event['Records']
+    data = [decode_message(json.dumps(record['kinesis']['data'])) for record in records]
+    rdd = spark.sparkContext.parallelize(data)
+
+    schema = StructType([StructField(column, StringType()) for column in COLUMNS])
+
+    model = PipelineModel.load(PATH_TO_MODEL)
+    s3_bucket_name = S3_BUCKET
+
+    df = spark.read.json(rdd, schema=schema)
+    if df.count() == 0:
+        return {'statusCode': 200, 'body': 'OK'}
+    ids = df.select('customerID').rdd.flatMap(lambda x: x).collect()
+    data = transform_dataset(df)
+    pred = model.transform(data)
+    pred = pred.select('prediction').rdd.flatMap(lambda x: x).collect()
+    res = {id_: churn for id_, churn in zip(ids, pred)}
+    s3 = boto3.resource('s3')
+    s3.Object(s3_bucket_name, f'prediction_{time.time()}.json').put(
+        Body=(bytes(json.dumps(res).encode('UTF-8')))
     )
-
-    runner.run(trigger_once=True)
-
     return {'statusCode': 200, 'body': 'OK'}
